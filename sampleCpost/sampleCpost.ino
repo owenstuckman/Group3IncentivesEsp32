a2f1e6cc049672bc6728d99fbb9b34f23dae2066
owenstuckman <ostuckman@outlook.com>
2024-11-13 16:09:11 -0500

    Code Cleanup and Commenting

--- sampleCpost/sampleCpost.ino
+++ sampleCpost/sampleCpost.ino
@@ -1,31 +1,51 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+/*
 
-#define LED 2  // Define LED pin
+Working code to connect to WiFi with an ESP32 board and post to our Supabase Database.
+Inserts a row into 'Scanned Items'
+Idea is for this post to send the HokieP and the barcode scanned into the database. 
 
-const char* ssid = "VT Open WiFi";  // Name of the network
+Written by Owen Stuckman on 11/13/2024. 
 
-// API endpoint
+*/
+
+#include <WiFi.h> // Wifi library
+#include <HTTPClient.h> // Http client
+
+// Define LED pin (on board)
+#define LED 2  
+
+// Name of the network
+const char* ssid = "VT Open WiFi";  
+// API endpoint (Supabase Table)
 const char* serverUrl = "https://zmqjskgfggxxmpfhygni.supabase.co/rest/v1/ScannedItems";
-// API key
+// API key (Supabase)
 const char* apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptcWpza2dmZ2d4eG1wZmh5Z25pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzODM1NTcsImV4cCI6MjA0NTk1OTU1N30.So0-6hvuRcrW89GkzIOdaQhkA0k22QlFc4ev3zKSgqY";
 
+// Run before Loop() is run
 void setup() {
+
+    // set baud rate to 115200
     Serial.begin(115200);
+    // setup on board led
     pinMode(LED, OUTPUT);
 
-    WiFi.mode(WIFI_STA);  // Station mode
-    WiFi.begin(ssid);      // Connect to open Wi-Fi
+    // Initialize ESP32 to station mode (connecting)
+    WiFi.mode(WIFI_STA);  
+    // Connect to specifies network
+    WiFi.begin(ssid);      
 
+    // init vars
     int attempts = 0;
     const int maxAttempts = 10;
 
+    // Check, then wait
     while (WiFi.status() != WL_CONNECTED && attempts < maxAttempts) {
         delay(500);
         Serial.print(".");
         attempts++; 
     }
 
+    // State whether was able to connect or not
     if (WiFi.status() == WL_CONNECTED) {
         Serial.println("\nConnected to Wi-Fi\n\n\n");
     } else {
@@ -33,6 +53,8 @@ void setup() {
        
     }
 
+
+    // Init http client, and start request to server
     HTTPClient http;
     http.begin(serverUrl);
 
@@ -43,10 +65,10 @@ void setup() {
     // Create JSON payload
     String payload = "{\"sample\":\"John Doe\"}";
 
-    // Send HTTP POST request
+    // Send POST request
     int httpResponseCode = http.POST(payload);
 
-    // Check HTTP response
+    // Check HTTP response, print errors to serial monitor
     if (httpResponseCode > 0) {
         String response = http.getString();
         Serial.println("HTTP Response code: " + String(httpResponseCode));
@@ -59,9 +81,10 @@ void setup() {
 }
 
 void loop() {
+    // Flash LED to have physical indication that setup is complete (for testing)
     digitalWrite(LED, HIGH);
     delay(3000);
     digitalWrite(LED, LOW);
     delay(3000);
-    Serial.println("Things are going");
+    Serial.println("Testing");
 }
